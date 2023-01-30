81e7275040d40ee0a33f7791f7d407bcff6e7fda
Fredrik Karlsson <fredrik.karlsson@ociusnetgroup.com>
2023-01-30 15:39:27 +0100

    Change location

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
        {
            name: 'Magnemite',
            location: {
-               lat: 44.496470,
-               lng: 11.320180,
+               lat: 59.529928,
+               lng: 15.988211,
            }
        },
    ];
